ae610ad35caf12b62abbfb5bcd68fbcb46d158db
superloyt <loisejiele@yahoo.com>
2023-09-22 23:44:44 +0800

    test webcam

--- src/views/screens/shared/webcam/views/index.tsx
+++ src/views/screens/shared/webcam/views/index.tsx
@@ -11,15 +11,17 @@ export default (props: IProps) => {
     <Webcam
       ref={webcamRef}
       audio={audio}
+      width={480}
+      height={720}
       mirrored={facingMode === 'user'}
       screenshotFormat={screenshotFormat}
       forceScreenshotSourceSize={forceScreenshotSourceSize}
       videoConstraints={{
-        width,
-        height,
+        width: { min: 480 },
+        height: { min: 720 },
         frameRate,
         facingMode,
-        aspectRatio,
+        aspectRatio: 0.6666666667,
       }}
     />
   );
