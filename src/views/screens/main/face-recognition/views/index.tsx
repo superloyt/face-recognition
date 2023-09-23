70d8e32b38ee73bef3039c818456b769b52ea936
superloyt <loisejiele@yahoo.com>
2023-09-23 17:02:30 +0800

    polish css

--- src/views/screens/main/face-recognition/views/index.tsx
+++ src/views/screens/main/face-recognition/views/index.tsx
@@ -13,38 +13,9 @@ export default (props: IProps) => {
     onDetectFace, onRetakeImage,
   } = props;
   return (
-    // <>
-    //   {isFaceLoading ? (
-    //     <Spin indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} />
-    //   ) : (
-    //     <>
-    //       <Row justify="center">
-    //         <img id="imagePreview" src={image} alt="Preview" style={{ width, height }} />
-    //         <canvas id="faceDetection" />
-    //       </Row>
-    //       <Row justify="center">
-    //         <Space.Compact>
-    //           <Button
-    //             icon={<CameraOutlined />}
-    //             onClick={onRetakeImage}
-    //           >
-    //             Retake Image
-    //           </Button>
-    //           <Button
-    //             type="primary"
-    //             icon={<SmileOutlined />}
-    //             onClick={onDetectFace}
-    //           >
-    //             Detect Face
-    //           </Button>
-    //         </Space.Compact>
-    //       </Row>
-    //     </>
-    //   )}
-    // </>
     <>
       <Row justify="center">
-        <img id="imagePreview" src={image} alt="Preview" style={{ width, height }} />
+        <img id="imagePreview" src={image} alt="Preview" style={{ width: `${width}px`, height: `${height}px` }} />
         <canvas id="faceDetection" />
       </Row>
       <Row justify="center">
