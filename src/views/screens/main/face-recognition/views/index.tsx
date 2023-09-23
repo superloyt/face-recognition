e07a776475d71a0c853b259673af5c0b2d972d32
superloyt <loisejiele@yahoo.com>
2023-09-23 17:07:24 +0800

    polish css

--- src/views/screens/main/face-recognition/views/index.tsx
+++ src/views/screens/main/face-recognition/views/index.tsx
@@ -15,7 +15,7 @@ export default (props: IProps) => {
   return (
     <>
       <Row justify="center">
-        <img id="imagePreview" src={image} alt="Preview" style={{ width: `${width}px`, height: `${height}px` }} />
+        <img id="imagePreview" src={image} alt="Preview" style={{ width: `${width}px`, height: `${height}px`, objectFit: 'cover' }} />
         <canvas id="faceDetection" />
       </Row>
       <Row justify="center">
