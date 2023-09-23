2620c44e39c3122c5207fa9184306e1819461a18
superloyt <loisejiele@yahoo.com>
2023-09-23 16:46:53 +0800

    loading face-detect

--- src/views/screens/main/face-recognition/views/index.tsx
+++ src/views/screens/main/face-recognition/views/index.tsx
@@ -1,18 +1,47 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import {
-  Row, Button, Space,
+  Row, Button, Space, Spin,
 } from 'antd';
-import { CameraOutlined, SmileOutlined } from '@ant-design/icons';
+import { CameraOutlined, SmileOutlined, LoadingOutlined } from '@ant-design/icons';
 import { IProps } from './props';
 import './styles.css';
 
 export default (props: IProps) => {
   const {
-    image, width, height,
+    image, width, height, isFaceLoading,
     onDetectFace, onRetakeImage,
   } = props;
   return (
+    // <>
+    //   {isFaceLoading ? (
+    //     <Spin indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} />
+    //   ) : (
+    //     <>
+    //       <Row justify="center">
+    //         <img id="imagePreview" src={image} alt="Preview" style={{ width, height }} />
+    //         <canvas id="faceDetection" />
+    //       </Row>
+    //       <Row justify="center">
+    //         <Space.Compact>
+    //           <Button
+    //             icon={<CameraOutlined />}
+    //             onClick={onRetakeImage}
+    //           >
+    //             Retake Image
+    //           </Button>
+    //           <Button
+    //             type="primary"
+    //             icon={<SmileOutlined />}
+    //             onClick={onDetectFace}
+    //           >
+    //             Detect Face
+    //           </Button>
+    //         </Space.Compact>
+    //       </Row>
+    //     </>
+    //   )}
+    // </>
     <>
       <Row justify="center">
         <img id="imagePreview" src={image} alt="Preview" style={{ width, height }} />
@@ -30,6 +59,7 @@ export default (props: IProps) => {
             type="primary"
             icon={<SmileOutlined />}
             onClick={onDetectFace}
+            loading={isFaceLoading}
           >
             Detect Face
           </Button>
